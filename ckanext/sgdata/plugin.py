20f413de06805407144b3c57383f2a90b7f4d7e3
Sean Hammond <git@seanh.cc>
2014-07-29 14:55:14 +0000

    Rename "Description" dataset field to "Purpose"
    Web UI only, it'll still be called "notes" in the API.

---
+++ ckanext/sgdata/plugin.py
@@ -0,0 +1,11 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class SGDatasetForm(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
