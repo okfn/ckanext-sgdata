a3a49224ecedf59753f0885e2e7fd3643d0ff8ee
Sean Hammond <git@seanh.cc>
2014-08-03 18:21:53 +0000

    First version of custom package form
    * Override the package_create and package_update actions so we can do
      some custom validation and change error messages
    
    * Remove stage 3 of the three-stage dataset creation process (CSS and
      controller change)
    
    * Override package_form.html with our own package form. Most of the
      fields are still disabled and not implemented.

--- ckanext/sgdata/plugin.py
+++ ckanext/sgdata/plugin.py
@@ -1,15 +1,72 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.lib.helpers as helpers
+
+
+def _change_error_dict(err):
+    errors = err.error_dict.copy()
+
+    # Change "Tag string" to "Keywords" in the error summary shown to the user.
+    if 'tag_string' in errors:
+        errors['keywords'] = errors['tag_string']
+        del errors['tag_string']
+
+    # Change "Name" to "URL" in the error summary shown to the user.
+    if 'name' in errors:
+        errors['URL'] = errors['name']
+        del errors['name']
+
+    return errors
+
+def package_create(context, data_dict):
+    import ckan.logic.action.create
+
+    error_dict = {}
+
+    if not data_dict.get('tags'):
+        error_dict['keywords'] = ['Missing value']
+
+    try:
+        result = ckan.logic.action.create.package_create(context, data_dict)
+    except toolkit.ValidationError as err:
+        error_dict.update(_change_error_dict(err))
+
+    if error_dict:
+        raise toolkit.ValidationError(error_dict)
+
+    return result
+
+
+def package_update(context, data_dict):
+    import ckan.logic.action.update
+
+    error_dict = {}
+
+    if not data_dict.get('tags'):
+        error_dict['keywords'] = ['Missing value']
+
+    try:
+        result = ckan.logic.action.update.package_update(context, data_dict)
+    except toolkit.ValidationError as err:
+        error_dict.update(_change_error_dict(err))
+
+    if error_dict:
+        raise toolkit.ValidationError(error_dict)
+
+    return result
 
 
 class SGDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IDatasetForm)
+    plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
 
     # IDatasetForm
 
@@ -20,11 +77,14 @@ class SGDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         return ('dataset',)
 
     def _customize_package_schema(self, schema):
-        schema['administrative_source'] = [
-            toolkit.get_validator('not_empty'),
+        schema['title'] = [
             toolkit.get_validator('not_missing'),
+            toolkit.get_validator('not_empty'),
+            unicode]
+        schema['administrative_source'] = [
+            toolkit.get_validator('not_missing'),
+            toolkit.get_validator('not_empty'),
             toolkit.get_converter('convert_to_extras')]
-        return schema
 
     def create_package_schema(self):
         schema = super(SGDatasetForm, self).create_package_schema()
@@ -41,3 +101,43 @@ class SGDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         schema['administrative_source'] = [
             toolkit.get_converter('convert_from_extras')]
         return schema
+
+    # IRoutes
+
+    def before_map(self, map_):
+
+        # Override the package controller's new_metadata() method so we can
+        # skip the "additional info" stage of the three-stage dataset creation
+        # process and go straight to creating the dataset.
+        map_.connect(
+            '/dataset/new_metadata/{id}',
+            controller='ckanext.sgdata.plugin:SGDataPackageController',
+            action='new_metadata')
+
+        return map_
+
+    # IActions
+
+    def get_actions(self):
+        return {'package_create': package_create,
+                'package_update': package_update,
+                }
+
+
+class SGDataPackageController(toolkit.BaseController):
+
+    def new_metadata(self, id, data=None, errors=None, error_summary=None):
+        import ckan.model as model
+        import ckan.lib.base as base
+
+        # Change the package state from draft to active and save it.
+        context = {'model': model, 'session': model.Session,
+                   'user': toolkit.c.user or toolkit.c.author,
+                   'auth_user_obj': toolkit.c.userobj}
+        data_dict = toolkit.get_action('package_show')(context, {'id': id})
+        data_dict['id'] = id
+        data_dict['state'] = 'active'
+        toolkit.get_action('package_update')(context, data_dict)
+
+        base.redirect(helpers.url_for(controller='package', action='read',
+                                      id=id))
