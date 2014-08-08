371c5b30e04ec4a258d9f2cefc7c307c2af2baf3
Sam Smith <sam@mintcanary.com>
2014-08-08 12:56:23 +0100

    add resources

--- ckanext/sgdata/plugin.py
+++ ckanext/sgdata/plugin.py
@@ -227,6 +227,7 @@ class SGDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('resources', 'theme')
 
     # IDatasetForm
 
